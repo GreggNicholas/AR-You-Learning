05cf428687f2c903c287d1c85e50a0d8bab75b24
kelveenfabian <kelveenfabian@pursuit.org>
2019-05-11 22:39:50 -0400

    RecyclerView: Create CategoryViewHolder class

---
+++ app/src/main/java/com/example/aryoulearning/view/CategoryViewHolder.java
@@ -0,0 +1,18 @@
+package com.example.aryoulearning.view;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.example.aryoulearning.model.AnimalModel;
+
+public class CategoryViewHolder extends RecyclerView.ViewHolder {
+
+    public CategoryViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    public void onBind(final AnimalModel animalModel){
+
+    }
+}
