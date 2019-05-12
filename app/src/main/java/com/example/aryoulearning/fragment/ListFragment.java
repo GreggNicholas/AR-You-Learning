c2a3a56040c83259f7a116c618e1ea86cbdadf7d
Krishan Roy <krishanroy@pursuit.org>
2019-05-12 11:49:43 -0400

    add list fragment and move everything to the list fragment

---
+++ app/src/main/java/com/example/aryoulearning/fragment/ListFragment.java
@@ -0,0 +1,28 @@
+package com.example.aryoulearning.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.aryoulearning.R;
+
+public class ListFragment extends Fragment {
+    public static ListFragment newInstance(){
+        return new ListFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
