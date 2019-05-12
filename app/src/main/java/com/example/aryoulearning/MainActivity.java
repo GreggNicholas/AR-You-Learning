f8bc65f05c35623ecabb5d5e7d6ef69e8fc81851
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-05-11 23:35:57 -0400

    Edit retrofitcall in  main activity

--- app/src/main/java/com/example/aryoulearning/MainActivity.java
+++ app/src/main/java/com/example/aryoulearning/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.example.aryoulearning.Model.AnimalModel;
+import com.example.aryoulearning.Model.AnimalList;
 import com.example.aryoulearning.Network.AnimalService;
 import com.example.aryoulearning.Network.RetrofitSingleton;
 
@@ -24,14 +24,14 @@ public class MainActivity extends AppCompatActivity {
         RetrofitSingleton.getInstance()
                 .create(AnimalService.class)
                 .getAnimals()
-                .enqueue(new Callback<AnimalModel>() {
+                .enqueue(new Callback<AnimalList>() {
                     @Override
-                    public void onResponse(Call<AnimalModel> call, Response<AnimalModel> response) {
-                        Log.d(TAG, "onResponse: " + response.body().getAnimalName());
+                    public void onResponse(Call<AnimalList> call, Response<AnimalList> response) {
+                        Log.d(TAG, "onResponse: " + response.body().getAnimals().get(0).getAnimalName());
                     }
 
                     @Override
-                    public void onFailure(Call<AnimalModel> call, Throwable t) {
+                    public void onFailure(Call<AnimalList> call, Throwable t) {
                         Log.e(TAG, "onFailure: " + t.getMessage());
                     }
                 });
