84f155b78f7f833757f57454df1a45c1ac686dca
kelveenfabian <kelveenfabian@pursuit.org>
2019-05-11 23:26:13 -0400

    Initialized RecyclerView field in MainActivity

--- app/src/main/java/com/example/aryoulearning/MainActivity.java
+++ app/src/main/java/com/example/aryoulearning/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.aryoulearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.aryoulearning.controller.CategoryAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rv;
+    private CategoryAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeViews();
+    }
+
+    public void initializeViews(){
+        rv = findViewById(R.id.category_rv);
+    }
+
+    public void showRecyclerView(){
+//        adapter = new CategoryAdapter();
+        rv.setAdapter(adapter);
+        rv.setLayoutManager(new LinearLayoutManager(this));
     }
 }
