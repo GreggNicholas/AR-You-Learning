4ae558ebce7e01f7439b87a1046df40721871491
kelveenfabian <kelveenfabian@pursuit.org>
2019-05-12 13:09:51 -0400

    stashing

--- app/src/main/java/com/example/aryoulearning/model/AnimalModel.java
+++ app/src/main/java/com/example/aryoulearning/model/AnimalModel.java
@@ -1,19 +1,19 @@
 package com.example.aryoulearning.Model;
 
-public class AnimalModel {
+public final class AnimalModel {
     private String name;
     private String image;
-
-    public String getName() {
-        return name;
-    }
-
-    public String getImage() {
-        return image;
-    }
 
     public AnimalModel(String name, String image) {
         this.name = name;
         this.image = image;
+    }
+
+    public String getAnimalName() {
+        return name;
+    }
+
+    public String getAnimalImage() {
+        return image;
     }
 }
