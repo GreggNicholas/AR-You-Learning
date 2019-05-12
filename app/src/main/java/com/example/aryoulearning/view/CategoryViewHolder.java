dd61c2d6fc84bd244dedf4bcc3b3859b906ff5ee
kelveenfabian <kelveenfabian@pursuit.org>
2019-05-11 23:14:00 -0400

    Add TextView to CategoryViewHolder

--- app/src/main/java/com/example/aryoulearning/view/CategoryViewHolder.java
+++ app/src/main/java/com/example/aryoulearning/view/CategoryViewHolder.java
@@ -3,16 +3,20 @@ package com.example.aryoulearning.view;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
-import com.example.aryoulearning.model.AnimalModel;
+import com.example.aryoulearning.AnimalModel;
+import com.example.aryoulearning.R;
 
 public class CategoryViewHolder extends RecyclerView.ViewHolder {
+    private TextView animalName;
 
     public CategoryViewHolder(@NonNull View itemView) {
         super(itemView);
+        animalName = itemView.findViewById(R.id.animal_name);
     }
 
     public void onBind(final AnimalModel animalModel){
-
+        animalName.setText(animalModel.getName());
     }
 }
