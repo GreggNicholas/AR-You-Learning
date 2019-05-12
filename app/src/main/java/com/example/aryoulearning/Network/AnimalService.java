cdbe5122aa993ce29d5da44e43cfd760ab9472b9
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-05-11 23:10:44 -0400

    Made retrofitcall in main activity

--- app/src/main/java/com/example/aryoulearning/Network/AnimalService.java
+++ app/src/main/java/com/example/aryoulearning/Network/AnimalService.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 
 public interface AnimalService {
     String ENDPOINT = "kelveenfabian/75380ae0e467f513762454bbe49a6c2e/" +
-            "raw/b255a2a1b43130439a499c729111b3a14ed0cb41/category.json";
+            "raw/c58b558a32c656abec58575f90d2fcb9eda2d65b/category.json";
 
     @GET(ENDPOINT)
     Call<AnimalModel> getAnimals();
