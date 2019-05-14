994f92edeb496d2fdeb780af5f74982e0ed7c5ce
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-05-14 19:48:41 -0400

    Create resultsfragment

--- app/src/main/java/com/example/aryoulearning/controller/NavListener.java
+++ app/src/main/java/com/example/aryoulearning/controller/NavListener.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface NavListener {
     void moveToListFragment(List<List<Model>> animalResponseList, List<String> categoryName);
     void moveToGameFragment(List<Model> animalResponseList);
+//    void moveToResultsFragment();
 }
