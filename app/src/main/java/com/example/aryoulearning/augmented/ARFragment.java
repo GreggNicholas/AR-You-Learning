8c0fc3419826e009b554c2efb231517a086a10eb
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-05-18 23:24:59 -0400

    Add camera & AR permissions

--- app/src/main/java/com/example/aryoulearning/augmented/ARFragment.java
+++ app/src/main/java/com/example/aryoulearning/augmented/ARFragment.java
@@ -18,15 +18,7 @@ public class ARFragment extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 }
