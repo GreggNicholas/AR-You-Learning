d8ef7a7692eeb0a875fd52984f269a251c630c28
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-05-19 05:35:24 -0400

    Edit linearlayout in arfragmenthost xml

--- app/src/main/java/com/example/aryoulearning/augmented/ARFragmentHost.java
+++ app/src/main/java/com/example/aryoulearning/augmented/ARFragmentHost.java
@@ -1,28 +1,185 @@
 package com.example.aryoulearning.augmented;
 
+import android.graphics.Point;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.aryoulearning.R;
+import com.example.aryoulearning.augmented.model.ModelLoader;
+import com.example.aryoulearning.augmented.pointer.PointerDrawable;
 import com.google.ar.core.Anchor;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Trackable;
+import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.HitTestResult;
+import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
 
 public class ARFragmentHost extends AppCompatActivity {
     private ArFragment arFragment;
+    private PointerDrawable pointer = new PointerDrawable();
+    private boolean isTracking;
+    private boolean isHitting;
+    private ModelLoader modelLoader;
+    private ModelRenderable catRenderable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_arfragment);
+        setContentView(R.layout.activity_arfragment_host);
+        modelLoader = new ModelLoader(new WeakReference<>(this));
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        initializeGallery();
+
+        if (arFragment != null) {
+            arFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
+                arFragment.onUpdate(frameTime);
+                onUpdate();
+            });
+        }
+
+
+
+    }
+
+    public void addNodeToScene(Anchor anchor, ModelRenderable renderable) {
+
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
+        node.setOnTapListener(new Node.OnTapListener() {
+            @Override
+            public void onTap(HitTestResult hitTestResult, MotionEvent motionEvent) {
+                anchorNode.getAnchor().detach();
+            }
+        });
 
 
     }
 
     public void onException(Throwable throwable) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage(throwable.getMessage())
+                .setTitle("Codelab error!");
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
-    public void addNodeToScene(Anchor anchor, ModelRenderable renderable) {
+    private void initializeGallery() {
+        LinearLayout gallery = findViewById(R.id.gallery_layout);
+
+        ImageView dog = new ImageView(this);
+        dog.setImageResource(R.drawable.dog);
+        dog.setContentDescription("Dog");
+        dog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ARFragmentHost.this.addObject(Uri.parse("dog.sfb"));
+
+            }
+        });
+        gallery.addView(dog);
+
+
     }
+
+
+    private void addObject(Uri model) {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        Point pt = getScreenCenter();
+        List<HitResult> hits;
+
+
+        if (frame != null) {
+            hits = frame.hitTest(pt.x, pt.y);
+            for (HitResult hit : hits) {
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane && ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    modelLoader.loadModel(hit.createAnchor(), model);
+
+                    break;
+                }
+            }
+        }
+    }
+
+    private void onUpdate() {
+        boolean trackingChanged = updateTracking();
+        View contentView = findViewById(android.R.id.content);
+        if (trackingChanged) {
+            if (isTracking) {
+                contentView.getOverlay().add(pointer);
+            } else {
+                contentView.getOverlay().remove(pointer);
+            }
+            contentView.invalidate();
+        }
+
+        if (isTracking) {
+            boolean hitTestChanged = updateHitTest();
+            if (hitTestChanged) {
+                pointer.setEnabled(isHitting);
+                contentView.invalidate();
+            }
+        }
+    }
+
+    private boolean updateTracking() {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        boolean wasTracking = isTracking;
+        isTracking = frame != null && frame.getCamera().getTrackingState() == TrackingState.TRACKING;
+
+        return isTracking != wasTracking;
+    }
+
+    private boolean updateHitTest() {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        android.graphics.Point pt = getScreenCenter();
+        List<HitResult> hits;
+
+        boolean wasHitting = isHitting;
+        isHitting = false;
+
+        if (frame != null) {
+            hits = frame.hitTest(pt.x, pt.y);
+
+            for (HitResult hit : hits) {
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane && ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    isHitting = true;
+                    break;
+                }
+            }
+        }
+
+        return wasHitting != isHitting;
+    }
+
+    private Point getScreenCenter() {
+        View vw = findViewById(android.R.id.content);
+        return new android.graphics.Point(vw.getWidth() / 2, vw.getHeight() / 2);
+    }
+
 }
