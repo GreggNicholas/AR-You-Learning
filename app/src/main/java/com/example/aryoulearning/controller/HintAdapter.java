114bbc6a0ed24cf7134b3cc0a53b228b1a8b68ad
Krishan Roy <krishanroy@pursuit.org>
2019-05-24 14:23:58 -0400

    copy the hintViewHolder class to the HintAdapter class and delete seperate hintViewHolder class

--- app/src/main/java/com/example/aryoulearning/controller/HintAdapter.java
+++ app/src/main/java/com/example/aryoulearning/controller/HintAdapter.java
@@ -3,14 +3,18 @@ package com.example.aryoulearning.controller;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.aryoulearning.R;
 import com.example.aryoulearning.model.Model;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-public class HintAdapter extends RecyclerView.Adapter<HintViewHolder> {
+public class HintAdapter extends RecyclerView.Adapter<HintAdapter.HintViewHolder> {
     private List<Model> modelList;
 
     public HintAdapter(List<Model> hintModelList) {
@@ -31,5 +35,18 @@ public class HintAdapter extends RecyclerView.Adapter<HintViewHolder> {
     @Override
     public int getItemCount() {
         return modelList.size();
+    }
+
+    class HintViewHolder extends RecyclerView.ViewHolder {
+        public HintViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void onBind(Model model) {
+            ImageView imageView = itemView.findViewById(R.id.hint_fragment_image_view);
+            TextView textView = itemView.findViewById(R.id.hint_fragment_textview);
+            Picasso.get().load(model.getImage()).into(imageView);
+            textView.setText(model.getName());
+        }
     }
 }
