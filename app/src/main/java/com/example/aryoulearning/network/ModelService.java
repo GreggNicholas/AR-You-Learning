c119c1da7f5e4ba136462442449d3cdbbe819677
kelveenfabian <kelveenfabian@pursuit.org>
2019-06-11 20:35:24 -0400

    Modify FAB color from gray to orange

--- app/src/main/java/com/example/aryoulearning/network/ModelService.java
+++ app/src/main/java/com/example/aryoulearning/network/ModelService.java
@@ -8,7 +8,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ModelService {
-    String ENDPOINT = "kelveenfabian/75380ae0e467f513762454bbe49a6c2e/raw/94e0e678c0c5eeb0b32b3edc9a35fc090b93935b/category.json";
+    String ENDPOINT = "kelveenfabian/75380ae0e467f513762454bbe49a6c2e/raw/5f552b935eb97fb458489c83a2d3421a1f5d0590/category.json";
 
     @GET(ENDPOINT)
     Call<List<ModelResponse>> getModels();
