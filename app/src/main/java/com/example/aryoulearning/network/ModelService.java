4e00dc42ce85fa8eb2604d477faa2e2d0a05f131
kelveenfabian <kelveenfabian@pursuit.org>
2019-08-01 08:35:51 -0400

    Modify warning cards to cater to multiple devices

--- app/src/main/java/com/example/aryoulearning/network/ModelService.java
+++ app/src/main/java/com/example/aryoulearning/network/ModelService.java
@@ -8,7 +8,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ModelService {
-    String ENDPOINT = "kelveenfabian/75380ae0e467f513762454bbe49a6c2e/raw/5f552b935eb97fb458489c83a2d3421a1f5d0590/category.json";
+    String ENDPOINT = "kelveenfabian/75380ae0e467f513762454bbe49a6c2e/raw/7c745364e690cb292ee13eae0a157df6323e3a19/category.json";
 
     @GET(ENDPOINT)
     Call<List<ModelResponse>> getModels();
